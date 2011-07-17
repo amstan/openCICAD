afdc467635b6c9a37c1283bbd51bcddf332ca845
Alexandru Stan <alex@hypertriangle.com>
2011-07-17 17:56:19 -0400

    working low level

--- firmware/test/main.c
+++ firmware/test/main.c
@@ -3,7 +3,22 @@
 
 #define LED1 0
 #define LED2 6
+#define BUS  LED2
+#define LED  LED1
 #define S2   3
+
+inline unsigned char read_bus() {
+	return test_bit(P1IN,BUS);
+}
+
+inline void send_bus(unsigned char val) {
+	change_bit(P1OUT,LED,val);
+	change_bit(P1DIR,BUS,val);
+}
+
+inline unsigned char switch_pressed() {
+	return !test_bit(P1IN,S2);
+}
  
 int main(void)
 {
@@ -12,12 +27,18 @@ int main(void)
 	BCSCTL1 = CALBC1_16MHZ;
 	
 	//Configure ports
-	P1DIR=(1<<LED1)|(1<<LED2);
-	P1REN=(1<<S2);
-	P1OUT=(1<<LED2)|(1<<S2);
+	P1REN=0; P1OUT=0; P1DIR=0;
+	
+	set_bit(P1DIR,LED); //enable led
+	
+	//enable S2
+	set_bit(P1REN,S2);
+	set_bit(P1OUT,S2);
+	
+	//output all the time on the bus, only toggle the DIR
+	set_bit(P1OUT,BUS);
 	
 	while(1) {
-		change_bit(P1OUT,LED1,test_bit(P1IN,S2));
-		change_bit(P1OUT,LED2,test_bit(P1IN,S2));
+		send_bus(switch_pressed());
 	}
 }
